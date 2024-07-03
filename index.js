1590383cdad307164485b31fb1b4a043e0dff36c
DiegoGRoldán <123426763+DIEGOGARCIA601050@users.noreply.github.com>
2024-07-02 18:06:20 -0600

    Update function Hack

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 function Hack(arr = []) {
   arr.forEach(element => {
-    for (let i = 0; i < arr.length; i++) {
-        const elemento = arr[i];
-        for (let j = 0; j < arr.length; j++) {
-            const elementito = arr[j];
-            console.log(element, elemento, elementito);
-        }
-    }
-});
-}
+    arr.forEach(item => {
+      arr.forEach(think => {
+        console.log(element,item,think)
+      })
+    })
+  }
+)}
